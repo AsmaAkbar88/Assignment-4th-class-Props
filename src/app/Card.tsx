ec0a8cfb369dd02650cea50b389985ce7bac53da
Asma Akbar <masoomlarki297@gmail.com>
2024-10-13 16:38:47 +0500

    Initial commit from Create Next App

---
+++ src/app/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h1> Name: </h1>
+      <h2>Roll num:</h2>
+      <h1>Day: </h1>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+export default function iD_Card (): any{
+  return(
+    
+    <div>
+      <Card/>
+
+    </div>
+  )
+}
